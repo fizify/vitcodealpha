84c7543337a4dc554bdbd0a10388dab4007d6fb8
fizy <42074408+fizify@users.noreply.github.com>
2019-11-30 15:54:27 +0530

    Update fizy#9347.py

--- daily-questions/28-11-19/fizy#9347.py
+++ daily-questions/28-11-19/fizy#9347.py
@@ -1,13 +1,7 @@
-def trib(n):
-    if n < 3:
-        return n
-    return trib(n-1) + trib(n-2) + trib(n-3)
+n= int(input("n: "))
 
-def tri(n):
-    result = []
-    for i in range(0,n):
-        result.append(trib(n))
-    return result
+terms=[0]
+for i in range(n):
+    l=len(terms); terms.append(1) if len(terms)<=2 else terms.append(terms[l-1]+terms[l-2]+terms[l-3])
 
-n= int(input('n:'))
-print(tri(n))
+print(terms)
